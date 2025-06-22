8c5b3becb26f374176bce135aa92573be8e86058
JhuniorCq <114239158+JhuniorCq@users.noreply.github.com>
2025-06-22 13:58:35 -0500

    chore(firebase): Actualización de ruta

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,10 +1,14 @@
 import admin from "firebase-admin";
 import fs from "node:fs/promises";
 
-const serviceAccountKeyJSON = await fs.readFile(
-  "./credentials/serviceAccountKey.json",
-  "utf-8"
+const serviceAccountKeyPath = path.resolve(
+  "src",
+  "config",
+  "credentials",
+  "serviceAccountKey.json"
 );
+
+const serviceAccountKeyJSON = await fs.readFile(serviceAccountKeyPath, "utf-8");
 const serviceAccountKeyObject = JSON.parse(serviceAccountKeyJSON);
 
 admin.initializeApp({
